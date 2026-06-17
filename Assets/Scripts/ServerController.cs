ec167d80f0775f2d6aef8eafc11902b2c53950b9
Fernando Daniel Garcia <fernandodanielgarcia@upc.edu.ar>
2026-06-17 17:17:26 -0300

    Feature: main menu scene and persistent NetworkManager
    -Escena de menu creada
    -Se pasa entre escenas cuando inicia el juego

--- Assets/Scripts/ServerController.cs
+++ Assets/Scripts/ServerController.cs
@@ -1,6 +1,7 @@
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ServerController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class ServerController : MonoBehaviour
         if (Keyboard.current.hKey.wasPressedThisFrame)
         {
             NetworkManager.Singleton.StartHost();
+
+            NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
         }
         if (Keyboard.current.cKey.wasPressedThisFrame)
         {
